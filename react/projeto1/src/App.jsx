452b9a284f49a92ada64ff2439978b360c5e11c3
Germano <germanno.verde@gmail.com>
2023-08-03 07:30:56 -0300

    ii

---
+++ react/projeto1/src/App.jsx
@@ -0,0 +1,13 @@
+import Titulo from "./Titulo"
+
+function App(){
+
+  return <div>
+    <Titulo font='italic' cor = 'red' nome = 'Germano Ribeiro' /><Titulo />
+    <Titulo cor = 'blue' /><Titulo cor = 'gree'  />
+    <Titulo cor = 'yellow' /><Titulo cor = 'pink'/>
+
+
+  </div>
+}
+export default App
